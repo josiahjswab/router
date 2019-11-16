cc35a0aa6112969b875a701ab4b6133d4e65d602
Josiah Swab <josiahjswab@gmail.com>
2019-11-16 15:11:42 -0800

    Improves app view

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ export default function App() {
     return (
         <div className='app'>
             <div className='header'>
-                <h1>Lorem Ipsum</h1>
+                <h1>⚡️ Something New ✨</h1>
             </div>
             <div className='search row'>
                 <TextInput 
