3ea991d8e7956071e902b3b087baa9ac9d9c5b3b
Josiah Swab <josiahjswab@gmail.com>
2019-11-15 20:17:19 -0800

    app component is fully hooked

--- src/App.js
+++ src/App.js
@@ -44,61 +44,66 @@ export default function App() {
     
     
     // render(){
-        const [searchInput, setSearchInput] = useState('');
-        const [searchData, setSearchData] = useState([]);
-        
-        // var toggleDisplay = {};
-        // if (this.state.toggleSaved) {
-            //     toggleDisplay.display = 'none';
-            // }
+    const [searchInput, setSearchInput] = useState('');
+    const [searchData, setSearchData] = useState([]);
+    const [toggleSaved, setToggleSaved] = useState(false);
+    
+    var toggleDisplay = {};
+    if (toggleSaved) {
+            toggleDisplay.display = 'none';
+        }
 /**
  * Behold below an incredible console.log contraption to check the value after the async setState.
  * I will find a better way to do this later.
  */
-        const consoleThat = () => {
-            
-            setTimeout(function(){ console.log(searchData); }, 1000 );
-    
-        };
-    
-        useEffect(consoleThat, [searchData]);
+    const toggle = () => {
+        toggleSaved ? setToggleSaved(false) : setToggleSaved(true);
+    };
 
-        return (
-            <div className='app'>
-                <div className='header'>
-                    <h1>Lorem Ipsum</h1>
-                </div>
-                <div className='search row'>
-                    <div>
-                        <input type='text' name='searchInput' value={searchInput} onChange={ e => setSearchInput(e.target.value) } ></input>
-                        <button className='button' onClick={
-                            /**this.getSearch*/ 
-                            getSearch
-                        }>go</button>
-                    </div>
-                    <div className='end'>
-                        <button 
-                            className='button' 
-                            onClick={/**() => {
-                                this.state.toggleSaved ? 
-                                this.setState({toggleSaved: false}) : 
-                                this.setState({toggleSaved: true}) 
-                            }*/ null}
-                        >
-                            toggle 
-                        </button>
-                    </div>
-                </div>
-                <div className='saved-div' /**style={toggleDisplay}*/>
-                    {/* <ul>
-                        {FakeData.slice(0, 5).map((title, index) => (
-                            <li className='saved-title' key={title.mal_id}>{title.title}</li>
-                        ))}    
-                    </ul> */}
-                    
-                </div>
-                {/* <SearchResults searchData={this.state.searchData}/> */}
+    const consoleThat = () => {
+        
+        setTimeout(function(){ console.log(searchData); }, 1000 );
+
+    };
+
+    useEffect(consoleThat, [searchData]);
+
+    return (
+        <div className='app'>
+            <div className='header'>
+                <h1>Lorem Ipsum</h1>
             </div>
-        );
+            <div className='search row'>
+                <div>
+                    <input type='text' name='searchInput' value={searchInput} onChange={ e => setSearchInput(e.target.value) } ></input>
+                    <button className='button' onClick={
+                        /**this.getSearch*/ 
+                        getSearch
+                    }>go</button>
+                </div>
+                <div className='end'>
+                    <button 
+                        className='button' 
+                        onClick={/**() => {
+                            this.state.toggleSaved ? 
+                            this.setState({toggleSaved: false}) : 
+                            this.setState({toggleSaved: true}) 
+                        }*/ toggle}
+                    >
+                        toggle 
+                    </button>
+                </div>
+            </div>
+            <div className='saved-div' style={toggleDisplay}>
+                {/* <ul>
+                    {FakeData.slice(0, 5).map((title, index) => (
+                        <li className='saved-title' key={title.mal_id}>{title.title}</li>
+                    ))}    
+                </ul> */}
+                
+            </div>
+            {/* <SearchResults searchData={this.state.searchData}/> */}
+        </div>
+    );
     // }
 }
